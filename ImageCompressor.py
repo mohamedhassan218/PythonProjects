344ad1f83429960ad3305227212d050e1228b0d6
mohamedhassan218 <m.hassan.def@gmail.com>
2023-05-02 19:00:49 +0300

    Modify the ImageCompressor to work with more types of images

--- ImageCompressor.py
+++ ImageCompressor.py
@@ -7,7 +7,7 @@
         2- Open a file image located at file_path and store it in the img variable.
         3- Retrieve the size of the image (height, width).
         4- Resize the image and ask the user the write the name of the compressed image.
-    Note: if you want to compress an image with an extention that differs from 'jpg', you've to add your extention in line 22.
+    Note: that program works only with the image extensions that Pillow library provides.
 """
 import PIL
 from PIL import Image
@@ -16,9 +16,10 @@ from tkinter.filedialog import *
 def compressor():
     file_path = askopenfilename()
     img = PIL.Image.open(file_path)
+    file_extension = file_path.split('.')[-1]
     height, width = img.size
     img = img.resize((height, width), PIL.Image.BOX)
-    save_path = asksaveasfilename()
-    img.save(save_path + '.jpg')
+    save_path = asksaveasfilename(defaultextension=file_extension)
+    img.save(save_path, optimize = True)
 
 compressor()
